c32e766955a2996933ef62d5e1c2696d4b703eab
MutuaFranklin <franklin.mutua@student.moringaschool.com>
2021-06-25 19:30:04 +0300

    Jquery hide and show containers

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -5,3 +5,28 @@ $(document).ready(function(){
   })
 });    
 
+//Display and hide certain div classes
+$(document).ready(function(){
+  $("#topping-link").click(function(){
+    $(".pizza-toppings").show();
+    $(".pizza-crusts").hide();
+    $(".pizza-sizes").hide();
+
+  });
+
+  $("#crust-link").click(function(){
+    $(".pizza-toppings").hide();
+    $(".pizza-crusts").show();
+    $(".pizza-sizes").hide();
+
+  });
+
+  $("#size-link").click(function(){
+    $(".pizza-toppings").hide();
+    $(".pizza-crusts").hide();
+    $(".pizza-sizes").show();
+
+  });
+
+});
+
